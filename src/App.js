3f8869dc678a51307e3fba798706e4ef016d0b3a
mrwerner392 <mrwerner392@gmail.com>
2021-05-27 21:58:53 -0400

    Bug fix.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRuoter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 const App = props => (
