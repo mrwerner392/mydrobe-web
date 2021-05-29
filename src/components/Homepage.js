e7ebcff5e57b988da5fe1d6313e10c222aace5d7
mrwerner392 <mrwerner392@gmail.com>
2021-05-28 22:52:01 -0400

    Form data handling.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ModalAuth from './ModalAuth';
 
 const Homepage = props => {
   
+  const [showModalAuth, setShowModalAuth] = useState(true)
+
+  const handleCreateAccount = data => console.log(data)
+
+  const handleLogIn = data => console.log(data)
+
   return (
-    <ModalAuth />
+    <>
+      { showModalAuth && (
+        <ModalAuth
+          handleCreateAccount={ handleCreateAccount }
+          handleLogIn={ handleLogIn }
+        />
+      )}
+    </>
   )
   
 }
--- src/components/ModalAuth.js
+++ src/components/ModalAuth.js
@@ -2,21 +2,31 @@ import React, { useState} from 'react';
 import Modal from './Modal';
 import { InputGroup, FormControl } from 'react-bootstrap';
 
-const ModalAuth = props => {
-  
+const ModalAuth = ({ handleCreateAccount, handleLogIn }) => {
+
   const [formType, setFormType] = useState('create')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
 
   const FORM_DATA = {
     create: {
       title: 'Create Account',
       secondaryBtnText: 'Log In',
-      handlePrimaryBtnClick: () => 'handle create account',
+      handlePrimaryBtnClick: () => handleCreateAccount({
+        username,
+        password,
+        passwordConfirmation
+      }),
       handleSecondaryBtnClick: () => setFormType('login')
     },
     login: {
       title: 'Log In',
       secondaryBtnText: 'Create Account',
-      handlePrimaryBtnClick: () => 'handle create account',
+      handlePrimaryBtnClick: () => handleLogIn({
+        username,
+        password
+      }),
       handleSecondaryBtnClick: () => setFormType('create')
     }
   }
@@ -32,14 +42,28 @@ const ModalAuth = props => {
       handleSecondaryBtnClick={ formData.handleSecondaryBtnClick }
     >
       <InputGroup className='mb-3'>
-        <FormControl placeholder='username'></FormControl>
+        <FormControl
+          placeholder='username'
+          value={ username }
+          onChange={ event => setUsername(event.target.value) }
+        ></FormControl>
       </InputGroup>
       <InputGroup className='mb-3'>
-        <FormControl type='password' placeholder='password'></FormControl>
+        <FormControl
+          type='password'
+          placeholder='password'
+          value={ password }
+          onChange={ event => setPassword(event.target.value) }
+        ></FormControl>
       </InputGroup>
       { formType === 'create' && (
         <InputGroup className='mb-3'>
-          <FormControl type='password' placeholder='confirm password'></FormControl>
+          <FormControl
+            type='password'
+            placeholder='confirm password'
+            value={ passwordConfirmation }
+            onChange={ event => setPasswordConfirmation(event.target.value) }
+          ></FormControl>
         </InputGroup>
       )}
     </Modal>
