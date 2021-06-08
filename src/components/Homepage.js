53b2e1d01e576472272d856885f1fb1a1aec4771
mrwerner392 <mrwerner392@gmail.com>
2021-06-08 13:57:10 -0400

    Render navbar and content container.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import ModalAuth from './ModalAuth';
+import Navbar from './Navbar';
+import Content from './Content';
 
 const Homepage = props => {
   
-  const [showModalAuth, setShowModalAuth] = useState(true)
+  const [showModalAuth, setShowModalAuth] = useState(false)
+  const [contentType, setContentType] = useState('wardrobe')
 
   const handleLogIn = data => console.log(data)
 
@@ -14,6 +17,8 @@ const Homepage = props => {
           closeModalAuth={ () => setShowModalAuth(false) }
         />
       )}
+      <Navbar setContentType={ setContentType }/>
+      <Content contentType={ contentType }/>
     </>
   )
   
